c76310a626397f4ec8d92ab7f5f1414e13496347
1Zgp <36508793+1Zgp@users.noreply.github.com>
2021-06-14 23:08:03 +0300

    Create harbour-barwal-hu.ts

---
+++ translations/harbour-barwal-hu.ts
@@ -0,0 +1,186 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="21"/>
+        <source>About</source>
+        <translation>Névjegy</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="24"/>
+        <source>Info</source>
+        <translation>Infó</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="43"/>
+        <source>Version</source>
+        <translation>Verzió</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="48"/>
+        <source>Your wallet for all your scannable codes</source>
+        <translation>Saját tárca minden szkennelhető kódod számára</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="57"/>
+        <source>Author</source>
+        <translation>Készítette</translation>
+    </message>
+</context>
+<context>
+    <name>AddBarcodeGroupPage</name>
+    <message>
+        <location filename="../qml/pages/AddBarcodeGroupPage.qml" line="47"/>
+        <source>Create group</source>
+        <translation>Csoport létrehozása</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodeGroupPage.qml" line="54"/>
+        <source>Name</source>
+        <translation>Név</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodeGroupPage.qml" line="62"/>
+        <location filename="../qml/pages/AddBarcodeGroupPage.qml" line="72"/>
+        <source>Could not add group!</source>
+        <translation>A csoport nem adható hozzá!</translation>
+    </message>
+</context>
+<context>
+    <name>AddBarcodePage</name>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="42"/>
+        <source>Create code</source>
+        <translation>Kód létrehozása</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="47"/>
+        <source>Barcode type</source>
+        <translation>Vonalkód típusa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="340"/>
+        <source>Name</source>
+        <translation>Név</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="350"/>
+        <source>Description</source>
+        <translation>Leírás</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="359"/>
+        <source>Code</source>
+        <translation>Kód</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="374"/>
+        <source>Could not add barcode!</source>
+        <translation>A vonalkód nem adható hozzá!</translation>
+    </message>
+</context>
+<context>
+    <name>BarcodesPage</name>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="40"/>
+        <source>About</source>
+        <translation>Névjegy</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="44"/>
+        <source>Add code</source>
+        <translation>Kód hozzáadása</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="86"/>
+        <source>Edit</source>
+        <translation>Szerkesztés</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="100"/>
+        <source>Remove</source>
+        <translation>Eltávolítás</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="122"/>
+        <source>No barcodes defined</source>
+        <translation>Nincsenek vonalkódok meghatározva</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="123"/>
+        <source>Choose &quot;Add code&quot; from the pulley menu.</source>
+        <translation>Válaszd a &quot;Kód hozzáadása&quot; lehetőséget a lehúzható menüből.</translation>
+    </message>
+</context>
+<context>
+    <name>EditBarcodeGroupPage</name>
+    <message>
+        <location filename="../qml/pages/EditBarcodeGroupPage.qml" line="23"/>
+        <source>Name</source>
+        <translation>Név</translation>
+    </message>
+</context>
+<context>
+    <name>EditBarcodePage</name>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="23"/>
+        <source>Barcode type</source>
+        <translation>Vonalkód típusa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="317"/>
+        <source>Name</source>
+        <translation>Név</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="326"/>
+        <source>Description</source>
+        <translation>Leírás</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="335"/>
+        <source>Code</source>
+        <translation>Kód</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="35"/>
+        <source>About</source>
+        <translation>Névjegy</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="39"/>
+        <source>Add Group</source>
+        <translation>Csoport hozzáadása</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="64"/>
+        <source>Edit</source>
+        <translation>Szerkesztés</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="73"/>
+        <source>Remove</source>
+        <translation>Eltávolítás</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="96"/>
+        <source>No groups defined</source>
+        <translation>Nincsenek csoportok meghatározva</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="97"/>
+        <source>Choose &quot;Add Group&quot; from the pulley menu.</source>
+        <translation>Válaszd a &quot;Csoport hozzáadása&quot; lehetőséget a lehúzható menüből.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="31"/>
+        <source>BarWal</source>
+        <translation>BarWal</translation>
+    </message>
+</context>
+</TS>
