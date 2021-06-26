62f5aa2e6ce9974468e81da789319cd0dda8ad04
JSEHV <jorren.schauwaert@gmail.com>
2021-06-26 22:13:37 +0200

    Add Dutch translation
    Add Dutch translation

---
+++ translations/harbour-barwal-nl.ts
@@ -0,0 +1,264 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="20"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="23"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="42"/>
+        <source>Version</source>
+        <translation>Versie</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="47"/>
+        <source>Your wallet for all your scannable codes</source>
+        <translation>Jouw portemonnee voor alle scanbare codes</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="56"/>
+        <source>Author</source>
+        <translation>Auteur</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="73"/>
+        <source>Using </source>
+        <translation>Gebruikt </translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="73"/>
+        <source> as backend</source>
+        <translation> als backend</translation>
+    </message>
+</context>
+<context>
+    <name>AddBarcodeGroupPage</name>
+    <message>
+        <location filename="../qml/pages/AddBarcodeGroupPage.qml" line="43"/>
+        <source>Create group</source>
+        <translation>Groep toevoegen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodeGroupPage.qml" line="50"/>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodeGroupPage.qml" line="58"/>
+        <location filename="../qml/pages/AddBarcodeGroupPage.qml" line="68"/>
+        <source>Could not add group!</source>
+        <translation>Kan groep niet toevoegen!</translation>
+    </message>
+</context>
+<context>
+    <name>AddBarcodePage</name>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="77"/>
+        <source>Create code</source>
+        <translation>Code toevoegen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="82"/>
+        <source>Barcode type</source>
+        <translation>Code type</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="96"/>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="106"/>
+        <source>Description</source>
+        <translation>Omschrijving</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="115"/>
+        <source>Code</source>
+        <translation>Code</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="57"/>
+        <source>Could not add barcode!</source>
+        <translation>Kan code niet toevoegen!</translation>
+    </message>
+</context>
+<context>
+    <name>BarcodeInfoPage</name>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="29"/>
+        <source>Barcode info</source>
+        <translation>Code info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="42"/>
+        <source>Barcode name</source>
+        <translation>Code naam</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="63"/>
+        <source>Barcode description</source>
+        <translation>Code omschrijving</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="84"/>
+        <source>Barcode string</source>
+        <translation>Code reeks</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="126"/>
+        <source>Barcode type</source>
+        <translation>Code type</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="147"/>
+        <source>Referring zint code</source>
+        <translation>Verwijzende zint-code</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="105"/>
+        <source>Barcode group</source>
+        <translation>Code groep</translation>
+    </message>
+</context>
+<context>
+    <name>BarcodesPage</name>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="42"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="46"/>
+        <source>Add code</source>
+        <translation>Code toevoegen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="157"/>
+        <source>Edit</source>
+        <translation>Bewerken</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="171"/>
+        <source>Remove</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="141"/>
+        <source>Details</source>
+        <translation>Details</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="193"/>
+        <source>No barcodes defined</source>
+        <translation>Geen code gedefinieerd</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="194"/>
+        <source>Choose &quot;Add code&quot; from the pulley menu.</source>
+        <translation>Kies &quot;Code toevoegen&quot; in het menu.</translation>
+    </message>
+</context>
+<context>
+    <name>EditBarcodeGroupPage</name>
+    <message>
+        <location filename="../qml/pages/EditBarcodeGroupPage.qml" line="23"/>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+</context>
+<context>
+    <name>EditBarcodePage</name>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="95"/>
+        <source>Barcode type</source>
+        <translation>Code type</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="109"/>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="118"/>
+        <source>Description</source>
+        <translation>Omschrijving</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="127"/>
+        <source>Code</source>
+        <translation>Code</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="143"/>
+        <source>Import custom icon</source>
+        <translation>Importeer aangepast pictogram</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="152"/>
+        <source>Importing</source>
+        <translation>Importeren</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="161"/>
+        <source>&lt;ul&gt;&lt;li&gt;Will import &lt;b&gt;barwal_icon.png&lt;/b&gt; from Pictures&lt;/li&gt;&lt;li&gt;Icon has to be max 128x128px&lt;/li&gt;&lt;li&gt;Width should be exactly 128px&lt;/li&gt;&lt;/ul&gt;</source>
+        <translation>&lt;ul&gt;&lt;li&gt;Zal &lt;b&gt;barwal_icon.png&lt;/b&gt; importeren vanuit Pictures&lt;/li&gt;&lt;li&gt;Pictogram moet maximaal 128x128px zijn&lt;/li&gt;&lt;li&gt;Breedte moet exact 128px zijn&lt;/li&gt;&lt;/ul&gt;</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="169"/>
+        <source>Remove custom icon</source>
+        <translation>Verwijder aangepast pictogram</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="171"/>
+        <source>Removing</source>
+        <translation>Verwijderen</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="35"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="39"/>
+        <source>Add Group</source>
+        <translation>Groep toevoegen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="64"/>
+        <source>Edit</source>
+        <translation>Bewerken</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="73"/>
+        <source>Remove</source>
+        <translation>Verwijderen</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="96"/>
+        <source>No groups defined</source>
+        <translation>Geen groepen gedefinieerd</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="97"/>
+        <source>Choose &quot;Add Group&quot; from the pulley menu.</source>
+        <translation>Kids &quot;Groep toevoegen&quot; in het menu.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="31"/>
+        <source>BarWal</source>
+        <translation>BarWal</translation>
+    </message>
+</context>
+</TS>
