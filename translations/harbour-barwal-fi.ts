884159e28aea816a905aa61c780fe04bd88842b0
Elmeri Länsiharju <139926936+tuplasuhveli@users.noreply.github.com>
2025-01-06 23:35:22 +0000

    Create harbour-barwal-fi.ts
    Finnish translation

---
+++ translations/harbour-barwal-fi.ts
@@ -0,0 +1,312 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="20"/>
+        <source>About</source>
+        <translation>Tietoja</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="23"/>
+        <source>Info</source>
+        <translation>Info</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="42"/>
+        <source>Version</source>
+        <translation>Versio</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="47"/>
+        <source>Your wallet for all your scannable codes</source>
+        <translation>Lompakkosi kaikille viivakoodeillesi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="56"/>
+        <source>Author</source>
+        <translation>Tekijä</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="73"/>
+        <source>Using </source>
+        <translation>Taustaohjelmana käytetään</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="73"/>
+        <source> as backend</source>
+        <translation>-kirjastoa</translation>
+    </message>
+</context>
+<context>
+    <name>AddBarcodeGroupPage</name>
+    <message>
+        <location filename="../qml/pages/AddBarcodeGroupPage.qml" line="43"/>
+        <source>Create group</source>
+        <translation>Luo ryhmä</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodeGroupPage.qml" line="50"/>
+        <source>Name</source>
+        <translation>Nimi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodeGroupPage.qml" line="58"/>
+        <location filename="../qml/pages/AddBarcodeGroupPage.qml" line="68"/>
+        <source>Could not add group!</source>
+        <translation>Ryhmää ei voitu luoda!</translation>
+    </message>
+</context>
+<context>
+    <name>AddBarcodePage</name>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="77"/>
+        <source>Create code</source>
+        <translation>Luo viivakoodi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="82"/>
+        <source>Barcode type</source>
+        <translation>Viivakoodin tyyppi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="96"/>
+        <source>Name</source>
+        <translation>Nimi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="106"/>
+        <source>Description</source>
+        <translation>Kuvaus</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="115"/>
+        <source>Code</source>
+        <translation>Merkkijono</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/AddBarcodePage.qml" line="57"/>
+        <source>Could not add barcode!</source>
+        <translation>Viivakoodia ei voitu luoda!</translation>
+    </message>
+</context>
+<context>
+    <name>BarcodeGroupMovePage</name>
+    <message>
+        <location filename="../qml/pages/BarcodeGroupMovePage.qml" line="50"/>
+        <source>Move to group</source>
+        <translation>Siirrä ryhmään</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeGroupMovePage.qml" line="57"/>
+        <source>Moving</source>
+        <translation>Siirretään</translation>
+    </message>
+</context>
+<context>
+    <name>BarcodeInfoPage</name>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="30"/>
+        <source>Barcode info</source>
+        <translation>Viivakoodin tiedot</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="51"/>
+        <source>Barcode name</source>
+        <translation>Nimi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="72"/>
+        <source>Barcode description</source>
+        <translation>Kuvaus</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="93"/>
+        <source>Barcode string</source>
+        <translation>Merkkijono</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="135"/>
+        <source>Barcode type</source>
+        <translation>Tyyppi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="156"/>
+        <source>Referring zint code</source>
+        <translation>Zint-tyyppi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodeInfoPage.qml" line="114"/>
+        <source>Barcode group</source>
+        <translation>Ryhmä</translation>
+    </message>
+</context>
+<context>
+    <name>BarcodesPage</name>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="43"/>
+        <source>About</source>
+        <translation>Tietoja</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="47"/>
+        <source>Add code</source>
+        <translation>Lisää viivakoodi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="142"/>
+        <source>Details</source>
+        <translation>Tiedot</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="160"/>
+        <source>Edit</source>
+        <translation>Muokkaa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="176"/>
+        <source>Move</source>
+        <translation>Siirrä</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="185"/>
+        <source>Remove</source>
+        <translation>Poista</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="186"/>
+        <source>Deleting</source>
+        <translation>Poistetaan</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="207"/>
+        <source>No barcodes defined</source>
+        <translation>Viivakoodeja ei ole määritetty</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/BarcodesPage.qml" line="208"/>
+        <source>Choose &quot;Add code&quot; from the pulley menu.</source>
+        <translation>Valitse vetovalikosta &quot;Lisää viivakoodi&quot;.</translation>
+    </message>
+</context>
+<context>
+    <name>EditBarcodeGroupPage</name>
+    <message>
+        <location filename="../qml/pages/EditBarcodeGroupPage.qml" line="23"/>
+        <source>Name</source>
+        <translation>Nimi</translation>
+    </message>
+</context>
+<context>
+    <name>EditBarcodePage</name>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="96"/>
+        <source>Barcode type</source>
+        <translation>Viivakoodin tyyppi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="110"/>
+        <source>Name</source>
+        <translation>Nimi</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="119"/>
+        <source>Description</source>
+        <translation>Kuvaus</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="128"/>
+        <source>Code</source>
+        <translation>Merkkijono</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="145"/>
+        <source>Import custom icon</source>
+        <translation>Tuo mukautettu kuvake</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="155"/>
+        <source>Importing</source>
+        <translation>Tuodaan</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="165"/>
+        <source>&lt;ul&gt;&lt;li&gt;Will import &lt;b&gt;barwal_icon.png&lt;/b&gt; from Pictures&lt;/li&gt;&lt;li&gt;Icon has to be max 128x128px&lt;/li&gt;&lt;li&gt;Width should be exactly 128px&lt;/li&gt;&lt;/ul&gt;</source>
+        <translation>&lt;ul&gt;&lt;li&gt;Tuo Kuvista tiedoston &lt;b&gt;barwal_icon.png&lt;/b&gt;&lt;/li&gt;&lt;li&gt;Kuvakkeen enimmäiskoko on 128x128px&lt;/li&gt;&lt;li&gt;Leveyden tulee olla tasan 128px&lt;/li&gt;&lt;/ul&gt;</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="180"/>
+        <source>Remove custom icon</source>
+        <translation>Poista mukautettu kuvake</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/EditBarcodePage.qml" line="183"/>
+        <source>Removing</source>
+        <translation>Poistetaan</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="45"/>
+        <source>About</source>
+        <translation>Tietoja</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="49"/>
+        <source>Add Group</source>
+        <translation>Lisää ryhmä</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="55"/>
+        <source>Clear auto-open group</source>
+        <translation>Poista ryhmän automaattinen avaus</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="57"/>
+        <source>Removing</source>
+        <translation>Poistetaan</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="84"/>
+        <source>Edit</source>
+        <translation>Muokkaa</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="93"/>
+        <source>Remove</source>
+        <translation>Poista</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="94"/>
+        <source>Deleting</source>
+        <translation>Poistetaan</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="103"/>
+        <source>Auto open this group on start</source>
+        <translation>Avaa käynnistyksen yhteydessä</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="104"/>
+        <source>Auto open</source>
+        <translatio>Automaattinen avaus</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="147"/>
+        <source>No groups defined</source>
+        <translation>Ryhmiä ei ole määritetty</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="148"/>
+        <source>Choose &quot;Add Group&quot; from the pulley menu.</source>
+        <translation>Valitse vetovalikosta &quot;Lisää ryhmä&quot;.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="41"/>
+        <source>BarWal</source>
+        <translation>BarWal</translation>
+    </message>
+</context>
+</TS>
